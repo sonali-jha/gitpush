d632e5485713641c92c98d94b7a527c5b081c4d7
Bhanu <Bhanu@192.168.1.37>
2023-01-15 00:14:03 +0530

    Update

---
+++ java/src/main/java/JavaProgram/java/Sum.java
@@ -0,0 +1,14 @@
+package JavaProgram.java;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1,b=4;
+		int sum=0;
+		sum=1+2;
+		System.out.println("Sum of two number:"+sum);
+
+	}
+
+}
