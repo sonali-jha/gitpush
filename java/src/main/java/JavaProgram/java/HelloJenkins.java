ab6666c795d30eb92748fb60a42ad0e65144c355
Bhanu <Bhanu@192.168.1.37>
2023-01-15 08:49:52 +0530

    Update

---
+++ java/src/main/java/JavaProgram/java/HelloJenkins.java
@@ -0,0 +1,11 @@
+package JavaProgram.java;
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Jenkins");
+
+	}
+
+}
